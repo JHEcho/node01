e138ccf189bd9ee07f42cdc8763058fe32942987
ULdex <fcd1994@gmail.com>
2019-06-15 11:23:08 +0900

    no message

--- src/components/InfoBanner/InfoBanner.js
+++ src/components/InfoBanner/InfoBanner.js
@@ -3,11 +3,11 @@ import './InfoBanner.scss';
 
 export default function InfoBannerComponent() {
   return (
-    <div>matetest</div>
+    
     <div className="info-banner">
       <div className="row grid-default-style">
-      <div className="col-6"><h1 className="fon">text01</h1><p>subtest01</p></div>
-      <div className="col-6"><h1 className="fon">text02</h1><p>subtest02</p></div>
+      <div className="col-6 image01"><h1 className="fon">text01</h1><p>subtest01</p></div>
+      <div className="col-6 image02"><h1 className="fon">text02</h1><p>subtest02</p></div>
       </div>
     </div>
   );
